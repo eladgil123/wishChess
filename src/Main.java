2f85b4174bf9c9f8e346aa307ff1262963e41acc
Elad <eladgil2901@gmail.com>
2024-12-13 15:26:54 +0200

    queening fix

--- src/Main.java
+++ src/Main.java
@@ -78,6 +78,7 @@ public class Main {
 
             @Override
             public void mouseClicked(MouseEvent e) {
+                boolean isQ=false;
                 int x=e.getX()/100,y=e.getY()/100;
                 if(flip[0])
                     y=7-y;
@@ -105,23 +106,8 @@ public class Main {
                 if(board.board[lastClick/10][lastClick%10] instanceof Pawn&&(y==0||y==7)&&board.board[lastClick/10][lastClick%10].getMoves()[y][x]&&((movesC%2==0&&board.board[lastClick/10][lastClick%10].isWhite())||(movesC%2!=0&&!board.board[lastClick/10][lastClick%10].isWhite()))){
                     Queening q=new Queening(board.board[lastClick/10][lastClick%10].isWhite());
                     System.out.println();board.move(lastClick /10, lastClick %10,y,x,q.prom);
-                    movesC++;
-                    flip[0] =movesC%2==0;
-                    Main.setBoardLabels(bg,board, flip[0]);
-                    if(flip[0]) {
-                        for (int i = 0; i < 8; i++)
-                            for (int j = 0; j < 8; j++)
-                                bg[7 - i ][j].setDavid(false);
-                    }else{
-                        for (int i = 0; i < 8; i++)
-                            for (int j = 0; j < 8; j++)
-                                bg[i ][7-j].setDavid(false);
-                    }
+                    isQ=true;
 
-                    System.out.println(board.lastMove);
-                    lastClick =-1;
-
-                    return;
                 }
                 if(flip[0]) {
                     for (int i = 0; i < 8; i++)
@@ -132,24 +118,26 @@ public class Main {
                         for (int j = 0; j < 8; j++)
                             bg[i ][7-j].setDavid(false);
                 }
-                if(!board.move(lastClick /10, lastClick %10,y,x)) {
-                    if (board.board[y][x]!=null&&((movesC%2==0&&board.board[y][x].isWhite())||(movesC%2!=0&&!board.board[y][x].isWhite()))) {
-                        lastClick =(y)*10+x;
-                        board.board[y][x].reCheckMoves(board.board, y,x,board.wkl,board.bkl,board.lastMove);
-                        boolean[][] moves = board.board[y][x].getMoves();
-                        if(flip[0]) {
-                            for (int i = 0; i < 8; i++)
-                                for (int j = 0; j < 8; j++)
-                                    bg[7 - i ][j].setDavid(moves[i][j]);
-                        }else{
-                            for (int i = 0; i < 8; i++)
-                                for (int j = 0; j < 8; j++)
-                                    bg[i ][7- j].setDavid(moves[i][j]);
+                if(!isQ) {
+                    if (!board.move(lastClick / 10, lastClick % 10, y, x)) {
+                        if (board.board[y][x] != null && ((movesC % 2 == 0 && board.board[y][x].isWhite()) || (movesC % 2 != 0 && !board.board[y][x].isWhite()))) {
+                            lastClick = (y) * 10 + x;
+                            board.board[y][x].reCheckMoves(board.board, y, x, board.wkl, board.bkl, board.lastMove);
+                            boolean[][] moves = board.board[y][x].getMoves();
+                            if (flip[0]) {
+                                for (int i = 0; i < 8; i++)
+                                    for (int j = 0; j < 8; j++)
+                                        bg[7 - i][j].setDavid(moves[i][j]);
+                            } else {
+                                for (int i = 0; i < 8; i++)
+                                    for (int j = 0; j < 8; j++)
+                                        bg[i][7 - j].setDavid(moves[i][j]);
+                            }
+                            return;
                         }
+                        lastClick = -1;
                         return;
                     }
-                    lastClick =-1;
-                    return;
                 }
                 movesC++;
                 if((movesC%2==0&&!Utils.isThereMove(board,true))) {
